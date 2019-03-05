6f07c0beca9e7b79377c887b568973f420207ad9
MacStudent <macstudent@126-iMac11.local>
2019-03-04 21:22:01 -0500

    ....

--- day1IosPractice/ViewController.swift
+++ day1IosPractice/ViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var text1: UITextField!
+    @IBOutlet weak var text2: UITextField!
+    @IBOutlet weak var outputText: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
     }
-
-
+    
+    @IBAction func btnAdd(_ sender: UIButton) {
+        var a = Int(text1.text!)
+        var b = Int(text2.text!)
+        
+        
+        outputText.text = "\(a+b)"
+        
+    }
+    
+    @IBAction func btnSub(_ sender: UIButton) {
+        
+        
+        
+    }
+    
+    
+    @IBAction func btnMul(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func btnDiv(_ sender: UIButton) {
+    }
+    
+    
 }
 

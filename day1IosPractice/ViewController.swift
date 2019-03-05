bc560c959e1ee3f331e5c9195424570aad5700b5
MacStudent <macstudent@126-iMac12.local>
2019-03-05 18:16:41 -0500

    added float

--- day1IosPractice/ViewController.swift
+++ day1IosPractice/ViewController.swift
@@ -55,7 +55,7 @@ class ViewController: UIViewController {
         var b = Int(text2.text!)
         
         
-        outputText.text = "\((a!/b!))"
+        outputText.text = "\(Float(a!/b!))"
         
         
         
